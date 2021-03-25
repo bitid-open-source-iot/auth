2cb16bb2d5d07bd9c43ae373dbb69e644946614c
claytoncc <clayton@openiot.co.za>
2021-03-25 10:36:26 +0200

    updates

--- db/patches/patch_0001_cc/tblApps.sql
+++ db/patches/patch_0001_cc/tblApps.sql
@@ -2,249 +2,464 @@
 Set1 - Create tblApps including Unique index
 Set2 - Create AuditExact and Triggers
 Set3 - Insert initial record
-Set4 - Create stored procedure crud (fred)
+Set4 - Create stored procedure add
+Set5 - Create stored procedure get
+Set6 - Create stored procedure list
+Set7 - Create stored procedure update
+Set8 - Create stored procedure delete
 */
 
 -- drop table tblApps
 -- drop table tblApps_AuditExact
 
 -- Set1
-USE [auth]
-GO
+	USE [auth]
+	GO
 
-CREATE TABLE dbo.tblApps
-(
-	id INT NOT NULL IDENTITY(1, 1),
-	serverDate [datetime] DEFAULT getdate() NOT NULL,
-	userId Int NOT NULL,
-	appUrl VARCHAR(255) NOT NULL,
-	appName VARCHAR(255) NOT NULL,
-	icon VARCHAR(255) NOT NULL,
-	appSecret VARCHAR(255) NOT NULL,
-	themeColor VARCHAR(255) NOT NULL,
-	googleDatabase VARCHAR(255) DEFAULT (''),
-	themeBackground VARCHAR(255) NOT NULL,
-	googleCredentials VARCHAR(5000) DEFAULT ('{}'),
-	PRIMARY KEY (id)
-);
-CREATE UNIQUE INDEX tblAppsName ON dbo.tblApps (appName);
+	CREATE TABLE [dbo].[tblApps]
+	(
+		[id] INT NOT NULL IDENTITY(1, 1),
+		[userId] INT NOT NULL,
+		[serverDate] DATETIME NOT NULL DEFAULT getdate(),
+		[icon] VARCHAR(255) NOT NULL,
+		[appUrl] VARCHAR(255) NOT NULL,
+		[appName] VARCHAR(255) NOT NULL,
+		[appSecret] VARCHAR(255) NOT NULL,
+		[themeColor] VARCHAR(255) NOT NULL,
+		[googleDatabase] VARCHAR(255) DEFAULT (''),
+		[themeBackground] VARCHAR(255) NOT NULL,
+		[googleCredentials] VARCHAR(5000) DEFAULT ('{}'),
+		PRIMARY KEY (id)
+	);
+	CREATE UNIQUE INDEX tblAppsName ON [dbo].[tblApps] (appName);
 
 -- Set1
 
 -- Set2
 
+	PRINT 'Executing dbo.tblApps_AuditExact.TAB'
+	GO
 
+	USE [auth]
+	GO
 
-Print 'Executing dbo.tblApps_AuditExact.TAB'
-GO
-
-Use [auth]
-GO
-
-If Not Exists (Select Name
-from sys.objects
-where name = 'tblApps_AuditExact' and type = 'U')
-	Begin
-	Create Table tblApps_AuditExact
-	(
-		[id] Int IDENTITY (1, 1) NOT NULL,
-		[idOriginal] Int NOT NULL,
-		[userId] Int NOT NULL,
-		[userAction] Int NOT NULL,
-		[dateAction] DateTime Not NULL Constraint DF_tblApps_AuditExact_dateAction Default getdate(),
-		[appUrl] [varchar](255) NOT NULL,
-		[appName] [varchar](255) NOT NULL,
-		[icon] [varchar](255) NOT NULL,
-		[appSecret] [varchar](255) NOT NULL,
-		[themeColor] [varchar](255) NOT NULL,
-		[googleDatabase] [varchar](255) NOT NULL,
-		[themeBackground] [varchar](255) NOT NULL,
-		[googleCredentials] [varchar](255) NOT NULL,
-		Constraint PK_tblApps_AuditExact Primary Key Clustered (ID)
-	)
-End
-GO
-
-
--- Exec sp_Version @dboObject = 'dbo.tblApps_AuditExact.TAB', @Version = 1
--- Go
-
-
-
-
-Print 'Executing dbo.tr_tblApps_AuditExact.TRG'
-GO
-
--- sp_helptext tr_tblApps_AuditExact
-
-Use [auth]
-GO
-
-If Exists (Select *
-from sys.objects
-where name = 'tr_tblApps_AuditExact' and type = 'TR')
-Begin
-	Drop Trigger tr_tblApps_AuditExact
-End
-GO
-
-Create Trigger [dbo].[tr_tblApps_AuditExact]
-	on [dbo].[tblApps]
-	After Insert, Update, Delete
-	As
-	Begin
-
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-
-	--Insert
-	IF ((Select Count(ID)
-		From Inserted)) != 0 and ((Select Count(ID)
-		From Deleted) = 0)
+	IF NOT EXISTS (SELECT * FROM sys.objects WHERE name = 'tblApps_AuditExact' AND type = 'U')
 	BEGIN
-		Insert into tblApps_AuditExact
-			(idOriginal, userId, userAction, [appUrl], [appName], [icon], [appSecret], [themeColor], [googleDatabase], [themeBackground], [googleCredentials])
-		Select id, [userId], 1, [appUrl], [appName], [icon], [appSecret], [themeColor], [googleDatabase], [themeBackground], [googleCredentials]
-		From Inserted
+		CREATE TABLE tblApps_AuditExact
+		(
+			[id] INT IDENTITY (1, 1) NOT NULL,
+			[userId] INT NOT NULL,
+			[idOriginal] INT NOT NULL,
+			[userAction] INT NOT NULL,
+			[dateAction] DATETIME NOT NULL CONSTRAINT DF_tblApps_AuditExact_dateAction DEFAULT getdate(),
+			[icon] [varchar](255) NOT NULL,
+			[appUrl] [varchar](255) NOT NULL,
+			[appName] [varchar](255) NOT NULL,
+			[appSecret] [varchar](255) NOT NULL,
+			[themeColor] [varchar](255) NOT NULL,
+			[googleDatabase] [varchar](255) NOT NULL,
+			[themeBackground] [varchar](255) NOT NULL,
+			[googleCredentials] [varchar](255) NOT NULL,
+			CONSTRAINT PK_tblApps_AuditExact PRIMARY KEY CLUSTERED (ID)
+		)
 	END
+	GO
 
+	-- Exec sp_Version @dboObject = 'dbo.tblApps_AuditExact.TAB', @Version = 1
+	-- Go
 
-	--Update
-	IF ((Select Count(ID)
-		From Inserted)) != 0 and ((Select Count(ID)
-		From Deleted) != 0)
+	PRINT 'Executing dbo.tr_tblApps_AuditExact.TRG'
+	GO
+
+	-- sp_helptext tr_tblApps_AuditExact
+
+	USE [auth]
+	GO
+
+	IF EXISTS (SELECT * FROM sys.objects WHERE name = 'tr_tblApps_AuditExact' AND type = 'TR')
 	BEGIN
-		Insert into tblApps_AuditExact
-			(idOriginal, userId, userAction, [appUrl], [appName], [icon], [appSecret], [themeColor], [googleDatabase], [themeBackground], [googleCredentials])
-		Select id, [userId], 2, [appUrl], [appName], [icon], [appSecret], [themeColor], [googleDatabase], [themeBackground], [googleCredentials]
-		From Inserted
+		DROP TRIGGER tr_tblApps_AuditExact
 	END
+	GO
 
-	--Delete
-	IF ((Select Count(ID)
-		From Inserted)) = 0 and ((Select Count(ID)
-		From Deleted) != 0)
+	CREATE TRIGGER [dbo].[tr_tblApps_AuditExact]
+	ON [dbo].[tblApps]
+	AFTER INSERT, UPDATE, DELETE
+	AS
 	BEGIN
-		Insert into tblApps_AuditExact
-			(idOriginal, userId, userAction, [appUrl], [appName], [icon], [appSecret], [themeColor], [googleDatabase], [themeBackground], [googleCredentials])
-		Select id, [userId], 3, [appUrl], [appName], [icon], [appSecret], [themeColor], [googleDatabase], [themeBackground], [googleCredentials]
-		From Deleted
+
+		-- SET NOCOUNT ON added to prevent extra result sets FROM
+		-- interfering with SELECT statements.
+		SET NOCOUNT ON;
+
+		--Insert
+		IF ((SELECT Count(ID)
+			FROM Inserted)) != 0 AND ((SELECT Count(ID)
+			FROM Deleted) = 0)
+		BEGIN
+			INSERT INTO tblApps_AuditExact
+				(
+					[idOriginal],
+					[userId],
+					[userAction],
+					[icon],
+					[appUrl],
+					[appName],
+					[appSecret],
+					[themeColor],
+					[googleDatabase],
+					[themeBackground],
+					[googleCredentials]
+				)
+			SELECT
+				[id],
+				[userId],
+				1,
+				[icon],
+				[appUrl],
+				[appName],
+				[appSecret],
+				[themeColor],
+				[googleDatabase],
+				[themeBackground],
+				[googleCredentials]
+			FROM Inserted
+		END
+
+
+		--Update
+		IF ((SELECT Count(ID)
+			FROM Inserted)) != 0 AND ((SELECT Count(ID)
+			FROM Deleted) != 0)
+		BEGIN
+			INSERT INTO tblApps_AuditExact
+				(
+					[idOriginal],
+					[userId],
+					[userAction],
+					[icon],
+					[appUrl],
+					[appName],
+					[appSecret],
+					[themeColor],
+					[googleDatabase],
+					[themeBackground],
+					[googleCredentials]
+				)
+			SELECT
+				[id],
+				[userId],
+				2,
+				[icon],
+				[appUrl],
+				[appName],
+				[appSecret],
+				[themeColor],
+				[googleDatabase],
+				[themeBackground],
+				[googleCredentials]
+			FROM Inserted
+		END
+
+		--Delete
+		IF ((SELECT Count(ID)
+			FROM Inserted)) = 0 AND ((SELECT Count(ID)
+			FROM Deleted) != 0)
+		BEGIN
+			Insert into tblApps_AuditExact
+				(
+					[idOriginal],
+					[userId],
+					[userAction],
+					[icon],
+					[appUrl],
+					[appName],
+					[appSecret],
+					[themeColor],
+					[googleDatabase],
+					[themeBackground],
+					[googleCredentials]
+				)
+			SELECT
+				[id],
+				[userId],
+				3,
+				[icon],
+				[appUrl],
+				[appName],
+				[appSecret],
+				[themeColor],
+				[googleDatabase],
+				[themeBackground],
+				[googleCredentials]
+			FROM Deleted
+		END
+
 	END
-
-End
-GO
-
--- exec sp_Version @dboObject = 'dbo.tr_tblApps_AuditExact.TRG', @Version = 1
--- Go
-
-
-
-
-
-
-
+	GO
 
 -- Set2
 
 -- Set3
 
-INSERT INTO dbo.tblApps
-	(
-	userId,
-	appUrl,
-	appName,
-	icon,
-	appSecret,
-	themeColor,
-	themeBackground,
-	googleDatabase,
-	googleCredentials)
-VALUES
-	(
-		12369,
-		'https://auth.bitid.co.za',
-		'auth',
-		'https://auth.bitid.co.za/assets/icons/icon-512x512.png',
-		'xxx',
-		'#FFFFFF',
-		'#000000',
-		'xxx',
-		'{"type": "xxx","auth_uri": "xxx","client_id": "xxx","token_uri": "xxx","project_id": "xxx","private_key": "xxx","client_appName": "xxx","private_key_id": "xxx","client_x509_cert_url": "xxx","auth_provider_x509_cert_url": "xxx"}'
-	);
+	INSERT INTO dbo.tblApps
+		(
+			icon,
+			userId,
+			appUrl,
+			appName,
+			appSecret,
+			themeColor,
+			googleDatabase,
+			themeBackground,
+			googleCredentials
+		)
+	VALUES
+		(
+			'https://auth.bitid.co.za',
+			12369,
+			'auth',
+			'https://auth.bitid.co.za/assets/icons/icon-512x512.png',
+			'xxx',
+			'#FFFFFF',
+			'xxx',
+			'#000000',
+			'{}'
+		);
 
 -- Set3
 
+-- Set4
+
+	PRINT 'Executing dbo.v1_tblApps_Add.PRC'
+	GO
+
+	IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_tblApps_Add' AND type = 'P')
+	BEGIN
+		DROP PROCEDURE [dbo].[v1_tblApps_Add]
+	END
+	GO
+
+	CREATE PROCEDURE [dbo].[v1_tblApps_Add]
+		@icon VARCHAR(255),
+		@userId INT,
+		@appUrl VARCHAR(255),
+		@appName VARCHAR(255),
+		@appSecret VARCHAR(255),
+		@themeColor VARCHAR(255),
+		@googleDatabase VARCHAR(255) = NULL,
+		@themeBackground VARCHAR(255),
+		@googleCredentials VARCHAR(5000) = NULL
+	AS
+
+	SET NOCOUNT ON
+
+	BEGIN TRY
+		BEGIN TRAN
+			INSERT INTO [dbo].[tblApps]
+				(
+					icon,
+					userId,
+					appUrl,
+					appName,
+					appSecret,
+					themeColor,
+					googleDatabase,
+					themeBackground,
+					googleCredentials
+				)
+			VALUES
+				(
+					@icon,
+					@userId,
+					@appUrl,
+					@appName,
+					@appSecret,
+					@themeColor,
+					@googleDatabase,
+					@themeBackground,
+					@googleCredentials
+				);
+		COMMIT TRAN
+
+		SELECT @@ROWCOUNT;
+		RETURN @@ROWCOUNT;
+
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRAN
+		SELECT Error_Message()
+		RETURN -69
+	END CATCH
+	GO
 
 -- Set4
 
-Print 'Executing dbo.v1_tblApps_Add.PRC'
-GO
+-- Set5
 
-If Exists (Select Name
-from sys.objects
-where name = 'v1_tblApps_Add' and type = 'P')
-Begin
-	Drop Procedure dbo.v1_tblApps_Add
-End
-GO
+	PRINT 'Executing dbo.v1_tblApps_Get.PRC'
+	GO
 
-Create Procedure dbo.v1_tblApps_Add
-	@userId Int,
-	@appUrl VARCHAR(255),
-	@appName VARCHAR(255),
-	@icon VARCHAR(255),
-	@appSecret VARCHAR(255),
-	@themeColor VARCHAR(255),
-	@themeBackground VARCHAR(255),
-	@googleDatabase VARCHAR(255) = NULL,
-	@googleCredentials VARCHAR(5000) = NULL
-As
+	IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_tblApps_Get' AND type = 'P')
+	BEGIN
+		DROP PROCEDURE [dbo].[v1_tblApps_Get]
+	END
+	GO
 
-Set NoCount On
+	CREATE PROCEDURE [dbo].[v1_tblApps_Get]
+		@appId INT,
+		@userId INT
+	AS
 
-Begin Try
-	Begin Tran
-	print @userId
-		INSERT INTO dbo.tblApps
-	(
-	userId,
-	appUrl,
-	appName,
-	icon,
-	appSecret,
-	themeColor,
-	themeBackground,
-	googleDatabase,
-	googleCredentials
-	)
-VALUES
-	(
-		@userId,
-		@appUrl,
-		@appName,
-		@icon,
-		@appSecret,
-		@themeColor,
-		@themeBackground,
-		@googleDatabase,
-		@googleCredentials
-		);
+	SET NOCOUNT ON
 
-	commit tran
+	BEGIN TRY
+		SELECT
+			icon,
+			userId,
+			appUrl,
+			appName,
+			appSecret,
+			themeColor,
+			googleDatabase,
+			themeBackground,
+			googleCredentials
+		FROM [dbo].[tblApps]
+		WHERE id = @appId
+	END TRY
 
-	SELECT @@ROWCOUNT;
-	RETURN @@ROWCOUNT;
+	BEGIN CATCH
+		SELECT Error_Message()
+		RETURN -69
+	END CATCH
+	GO
 
-End Try
+-- Set5
 
-Begin Catch
-	Rollback Tran
-	Select Error_Message()
-	Return -69
-	-- Select @ErrorNumber = Error_number(), @ErrorMessage = Error_Message() Select @strErrorParameters = N'@UserID = ' + Cast(@UserID as NVarchar) + N', @ID = ' + Cast(@ID as NVarchar) exec Admin_LogUnhandledException @ErrorSource = 'CheechAndChong', @strParameters = @strErrorParameters, @ErrorNumber = @ErrorNumber, @ErrorMessage = @ErrorMessage Select dbo.fncV1_LanguageLookup(76,(Select LanguageOption From tblUsers Where ID = @UserID)) Return -1
-End Catch
+-- Set6
 
-GO
+	PRINT 'Executing dbo.v1_tblApps_List.PRC'
+	GO
 
--- Set4
+	IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_tblApps_List' AND type = 'P')
+	BEGIN
+		DROP PROCEDURE [dbo].[v1_tblApps_List]
+	END
+	GO
+
+	CREATE PROCEDURE [dbo].[v1_tblApps_List]
+		@userId INT
+	AS
+
+	SET NOCOUNT ON
+
+	BEGIN TRY
+		SELECT
+			icon,
+			userId,
+			appUrl,
+			appName,
+			appSecret,
+			themeColor,
+			googleDatabase,
+			themeBackground,
+			googleCredentials
+		FROM [dbo].[tblApps]
+	END TRY
+
+	BEGIN CATCH
+		SELECT Error_Message()
+		RETURN -69
+	END CATCH
+	GO
+
+-- Set6
+
+-- Set7
+
+	PRINT 'Executing dbo.v1_tblApps_Update.PRC'
+	GO
+
+	IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_tblApps_Update' AND type = 'P')
+	BEGIN
+		DROP PROCEDURE [dbo].[v1_tblApps_Update]
+	END
+	GO
+
+	CREATE PROCEDURE [dbo].[v1_tblApps_Update]
+		@icon VARCHAR(255),
+		@appId INT,
+		@userId INT,
+		@appUrl VARCHAR(255),
+		@appName VARCHAR(255),
+		@appSecret VARCHAR(255),
+		@themeColor VARCHAR(255),
+		@googleDatabase VARCHAR(255) = NULL,
+		@themeBackground VARCHAR(255),
+		@googleCredentials VARCHAR(5000) = NULL
+	AS
+
+	SET NOCOUNT ON
+
+	BEGIN TRY
+		BEGIN TRAN
+			UPDATE [dbo].[tblApps]
+			SET
+				[icon] = @icon,
+				[userId] = @userId,
+				[appUrl] = @appUrl,
+				[appName] = @appName,
+				[appSecret] = @appSecret,
+				[themeColor] = @themeColor,
+				[googleDatabase] = @googleDatabase,
+				[themeBackground] = @themeBackground,
+				[googleCredentials] = @googleCredentials
+			WHERE
+				id = @appId
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRAN
+		SELECT Error_Message()
+		RETURN -69
+	END CATCH
+	GO
+
+-- Set7
+
+-- Set8
+
+	PRINT 'Executing dbo.v1_tblApps_Delete.PRC'
+	GO
+
+	IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_tblApps_Delete' AND type = 'P')
+	BEGIN
+		DROP PROCEDURE [dbo].[v1_tblApps_Delete]
+	END
+	GO
+
+	CREATE PROCEDURE [dbo].[v1_tblApps_Delete]
+		@appId INT,
+		@userId INT
+	AS
+
+	SET NOCOUNT ON
+
+	BEGIN TRY
+		BEGIN TRAN
+			DELETE FROM [dbo].[tblApps]
+			WHERE
+				id = @appId
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRAN
+		SELECT Error_Message()
+		RETURN -69
+	END CATCH
+	GO
+
+-- Set8
