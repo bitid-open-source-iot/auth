72f82e37e2de1babcbcd73dcca0932ab8c9c4fb6
claytoncc <clayton@openiot.co.za>
2021-03-29 17:15:46 +0200

    updates

--- db/procedures/tokens.sql
+++ db/procedures/tokens.sql
@@ -64,7 +64,7 @@ BEGIN TRY
 				5,
 				@userId,
 				@tokenId
-			);
+			)
 
 		SELECT @tokenId AS [_id]
 		RETURN
@@ -205,7 +205,7 @@ AS
 SET NOCOUNT ON
 
 BEGIN TRY
-	IF EXISTS (SELECT TOP 1 [id] FROM [dbo].[tblTokensUsers] WHERE [role] >= 4 AND [userId] = @adminId)
+	IF EXISTS (SELECT TOP 1 [id] FROM [dbo].[tblTokensUsers] WHERE [role] >= 4 AND [userId] = @adminId AND [tokenId] = @tokenId)
 	BEGIN
 		INSERT INTO [dbo].[tblTokensUsers]
 			(
@@ -234,12 +234,151 @@ GO
 -- Set4
 
 -- Set5
+
+PRINT 'Executing dbo.v1_Tokens_Update.PRC'
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_Tokens_Update' AND type = 'P')
+BEGIN
+	DROP PROCEDURE [dbo].[v1_Tokens_Update]
+END
+GO
+
+CREATE PROCEDURE [dbo].[v1_Tokens_Update]
+	@appId INT,
+	@userId INT,
+	@device VARCHAR(255),
+	@expiry DATETIME,
+	@tokenId INT,
+	@description VARCHAR(255)
+AS
+
+SET NOCOUNT ON
+
+BEGIN TRY
+	IF EXISTS (SELECT TOP 1 [id] FROM [dbo].[tblTokensUsers] WHERE [role] >= 2 AND [userId] = @userId AND [tokenId] = @tokenId)
+	BEGIN
+		INSERT INTO [dbo].[tblTokens]
+			(
+				[appId],
+				[device],
+				[expiry],
+				[description]
+			)
+		VALUES
+			(
+				@appId,
+				@device,
+				@expiry,
+				@description
+			)
+		
+		SELECT @@ROWCOUNT AS [n]
+		RETURN
+	END
+END TRY
+
+BEGIN CATCH
+	SELECT Error_Message()
+	RETURN -69
+END CATCH
+GO
+
 -- Set5
 
 -- Set6
+
+PRINT 'Executing dbo.v1_Tokens_Delete.PRC'
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_Tokens_Delete' AND type = 'P')
+BEGIN
+	DROP PROCEDURE [dbo].[v1_Tokens_Delete]
+END
+GO
+
+CREATE PROCEDURE [dbo].[v1_Tokens_Delete]
+	@userId INT,
+	@tokenId INT
+AS
+
+SET NOCOUNT ON
+
+BEGIN TRY
+	BEGIN TRAN
+	
+	IF EXISTS (SELECT TOP 1 [id] FROM [dbo].[tblTokensUsers] WHERE [role] = 5 AND [userId] = @userId AND [tokenId] = @tokenId)
+	BEGIN
+		DELETE FROM
+			[dbo].[tblTokens]
+		WHERE
+			[id] = @tokenId
+
+		DELETE FROM
+			[dbo].[tblTokensUsers]
+		WHERE
+			[tokenId] = @tokenId
+
+		DELETE FROM
+			[dbo].[tblTokensScopes]
+		WHERE
+			[tokenId] = @tokenId
+		
+		SELECT @@ROWCOUNT AS [n]
+		RETURN
+	END
+
+	COMMIT TRAN
+END TRY
+
+BEGIN CATCH
+	ROLLBACK TRAN
+	SELECT Error_Message()
+	RETURN -69
+END CATCH
+GO
+
 -- Set6
 
 -- Set7
+
+PRINT 'Executing dbo.v1_Tokens_Share.PRC'
+GO
+
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'v1_Tokens_Share' AND type = 'P')
+BEGIN
+	DROP PROCEDURE [dbo].[v1_Tokens_Share]
+END
+GO
+
+CREATE PROCEDURE [dbo].[v1_Tokens_Share]
+	@role INT,
+	@userId INT,
+	@adminId INT,
+	@tokenId INT
+AS
+
+SET NOCOUNT ON
+
+BEGIN TRY
+	IF EXISTS (SELECT TOP 1 [id] FROM [dbo].[tblTokensUsers] WHERE [role] >= 4 AND [userId] = @adminId AND [tokenId] = @tokenId)
+	BEGIN
+		DELETE FROM
+			[dbo].[tblTokensUsers]
+		WHERE
+			[userId] = @userId
+		
+		SELECT @@ROWCOUNT AS [n]
+		RETURN
+	END
+END TRY
+
+BEGIN CATCH
+	SELECT Error_Message()
+	RETURN -69
+END CATCH
+GO
+
 -- Set7
 
 -- Set8
