2abb02b0784df445cd4fce2a806b3983269dc0b7
Shane Bowyer <shane@bitid.co.za>
2024-11-29 08:46:48 +0200

    temp remove email and verify

--- emails/emails.js
+++ emails/emails.js
@@ -6,41 +6,43 @@ const tools = require('../lib/tools');
 exports.verify = (args) => {
     var deferred = Q.defer();
 
-    var config = JSON.parse(JSON.stringify(__settings.smtp));
-    delete config.from;
+    // var config = JSON.parse(JSON.stringify(__settings.smtp));
+    // delete config.from;
 
-    const transporter = nodemailer.createTransport(config);
+    // const transporter = nodemailer.createTransport(config);
 
-    transporter.use('compile', hbs({
-        'viewEngine': {
-            'extName': '.hbs',
-            'layoutsDir': __dirname + '/templates',
-            'partialsDir': __dirname + '/templates',
-            'defaultLayout': 'verify.hbs'
-        },
-        'extName': '.hbs',
-        'viewPath': __dirname + '/templates'
-    }));
+    // transporter.use('compile', hbs({
+    //     'viewEngine': {
+    //         'extName': '.hbs',
+    //         'layoutsDir': __dirname + '/templates',
+    //         'partialsDir': __dirname + '/templates',
+    //         'defaultLayout': 'verify.hbs'
+    //     },
+    //     'extName': '.hbs',
+    //     'viewPath': __dirname + '/templates'
+    // }));
 
-    transporter.sendMail({
-        'context': {
-            'name': [args.user.name.first, args.user.name.last].join(' '),
-            'code': args.user.code,
-            'verify': [__settings.client.auth, '/verify-account?email=', args.user.email, '&code=', args.user.code, '&appId=', args.app.appId, '&returl=', args.app.url, '/authenticate'].join(''),
-            'branding': __settings.branding
-        },
-        'to': __settings.production ? args.user.email : __settings.smtp.auth.user,
-        'from': __settings.smtp.from,
-        'subject': 'Verify Account',
-        'template': 'verify'
-    }, (error, info) => {
-        if (error) {
-            tools.log('error', 'error in emails.verify', error);
-        } else {
-            tools.log('info', 'info in emails.verify', info);
-        };
-        deferred.resolve(args);
-    });
+    // transporter.sendMail({
+    //     'context': {
+    //         'name': [args.user.name.first, args.user.name.last].join(' '),
+    //         'code': args.user.code,
+    //         'verify': [__settings.client.auth, '/verify-account?email=', args.user.email, '&code=', args.user.code, '&appId=', args.app.appId, '&returl=', args.app.url, '/authenticate'].join(''),
+    //         'branding': __settings.branding
+    //     },
+    //     'to': __settings.production ? args.user.email : __settings.smtp.auth.user,
+    //     'from': __settings.smtp.from,
+    //     'subject': 'Verify Account',
+    //     'template': 'verify'
+    // }, (error, info) => {
+    //     if (error) {
+    //         tools.log('error', 'error in emails.verify', error);
+    //     } else {
+    //         tools.log('info', 'info in emails.verify', info);
+    //     };
+    //     deferred.resolve(args);
+    // });
+
+    deferred.resolve(args);
 
     return deferred.promise;
 };
